d7772a3bd3b9f98025a29b4568fd7cc16650e6af
agnesedalbo <agnesedalbo@gmail.com>
2025-05-06 12:43:31 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -5,4 +5,14 @@ matogrosso_l5_1992219_lrg.jpg #l5 nome programma satellite, 19 febbraio 1992 di 
 setwd("C:/Users/magda/OneDrive/Documents/UNIBO/telerilevamento") #per settare cartella di lavoro (copio dalla cartella il percorso e poi campio lo slash)
 
 mato = rast("Matogrosso_l5_1992219_lrg.jpg") #per importare immagine 
+plot(mato)
+
+# Bande: 
+# banda 1 = NIR
+# banda 2 = rosso
+# banda 3 = verde
+
+plotRGB(mato, r=2, g=3, b=1) #associo a ogni componente una banda (tutto ciò che sarà blu sarà vegetazione)
+plotRGB(mato, r=2, g=1, b=3) #qui tutto ciò che è verde sarà vegetazione 
+plotRGB(mato, r=1, g=3, b=1) #qui metto il nir nella banda del rosso, quindi la vegetazione sarà rossa
 
